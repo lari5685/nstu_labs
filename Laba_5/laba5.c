0f63b2865ce46152be39024855763c8c56bb6d90
lari5685 <LarAnd5685@gmail.com>
2020-10-29 09:06:42 +0700

    Update Laba5

--- Laba_5/laba5.c
+++ Laba_5/laba5.c
@@ -73,6 +73,41 @@ double sum6(double x,double eps,int *m){
 	return s;
 }
 
+//7. arctg sn: -x*x*(2*n-3)/(2*n-1)
+double sum7(double x,double eps,int *m){
+	double s,sn;
+	int n; 
+	for(s = 0, sn = x, n = 2; fabs(sn) > eps; n++){
+		s += sn;
+		sn = -sn * x*x*(2*n-3)/(2*n-1);
+	}
+	*m = n;
+	return s;
+}
+
+//8. arctg sn: -(2*n-3)/(x*x*(2*n-1))
+double sum8(double x,double eps,int *m){
+	double s,sn;
+	int n; 
+	for(s = M_PI_2, sn = -1/x, n = 2; fabs(sn) > eps; n++){
+		s += sn;
+		sn = -sn * (2*n-3)/(x*x*(2*n-1));
+	}
+	*m = n;
+	return s;
+}
+
+//9. 1/(1+x)^2 sn: -x * (n+1) / n
+double sum9(double x,double eps,int *m){
+	double s,sn;
+	int n; 
+	for(s = 0, sn = 1, n = 1; fabs(sn) > eps; n++){
+		s += sn;
+		sn = -sn * x * (n+1) / n;
+	}
+	*m = n;
+	return s;
+}
 
 int main(){ 
 	int nn;
@@ -89,4 +124,10 @@ int main(){
 		printf("n=%d \t x=%0.1f \t sum=%0.4lf \t sh=%0.4lf\n", nn,x, sum5(x, 0.0000001, &nn), sinh(x));
 	for (x=0; x <= 10; x += 0.1)
 		printf("n=%d \t x=%0.1f \t sum=%0.4lf \t ch=%0.4lf\n", nn,x, sum6(x, 0.0000001, &nn), cosh(x));
+	for (x=0; x <= 1; x += 0.1)
+		printf("n=%d \t x=%0.1f \t sum=%0.4lf \t atan=%0.4lf\n", nn,x, sum7(x, 0.0000001, &nn), atan(x));
+	for (x=1; x <= 5; x += 0.1)
+		printf("n=%d \t x=%0.1f \t sum=%0.4lf \t atan=%0.4lf\n", nn,x, sum8(x, 0.0000001, &nn), atan(x));
+	for (x=-0.9; x <= 0.9; x += 0.1)
+		printf("n=%d \t x=%0.1f \t sum=%0.4lf \t (x+1)^-2=%0.4lf\n", nn,x, sum9(x, 0.0000001, &nn), 1/((x+1)*(x+1)));
 }
