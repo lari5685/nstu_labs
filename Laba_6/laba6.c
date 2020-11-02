279cec51972e406bcd5b243b4ff6e792ce831826
lari5685 <LarAnd5685@gmail.com>
2020-11-03 01:01:10 +0700

    Releas Laba6

--- Laba_6/laba6.c
+++ Laba_6/laba6.c
@@ -3,56 +3,38 @@
 #include <math.h>
 
 
-int main(){
-	char str[20];
-	char out[50];
-
-	printf("input :: \n"); 
-	strcpy(str, "abc13,4defg2ho");
-	//scanf("%30s",str);
-
-	int l, k ,a = 0, b = 0, n = strlen(str);
-
-	for(int i = 0; i<n; i++){
-		if (fabs(str[i] - 53) <= 4){
-			l = i;
-			a = str[i] - 48;
+int main() {
+	char str[]= "abc2,3defg2ho";
+	char out[100];
+	gets(str);
+	int p=0, a = 0, b = 0; 
+	puts(str);
+	for (int i = 0; str[i] != '\0'; i++, p++) {
+		if (fabs(str[i] - 53) <= 4) {
+			a = str[i] - 48; b = 1;
 			i++;
-			while(str[i]>= 48 && str[i] <= 57){
-				a = a*10+(str[i]-48);
+			while (str[i] > 47 && str[i] < 58) {
+				a = a * 10 + str[i] - 48;
 				i++;
 			}
-
-			//have ','
-			if (str[i] == ',' && fabs(str[i+1] - 53) <= 4){
-				b = str[i+1] - 48;
-				i+=2;
-				while(str[i]>= 48 && str[i] <= 57){
-					b = b*10+(str[i]-48);
+			if (str[i] == ',' && fabs(str[i + 1] - 53) <= 4) {
+				b = str[++i] - 48;
+				i++;
+				while (str[i] > 47 && str[i] < 58) {
+					b = b * 10 + str[i] - 48;
 					i++;
 				}
-			
-				// we have n1(a) and n2(b)
-				printf("%d %d\n", a,b);
-				k = i;
-				
-				for(int j = 0; j<i; j++){
-					out[j] = str[j];
-				}
-
-
-
-
-
-				puts(s);
-			
 			}
-			// haven`t ',' // we have n1
-
+			for (int j = 0; j < a; j++) 
+				for (int m = 0; m < b; m++) {
+					out[p] = str[i+m];
+					p++;
+				}
+			i += b;
 		}
+		out[p] = str[i];
 	}
-
-	//scanf("%30s",str);
+	for (int i = 0; i<p; i++)
+		putchar(out[i]);
+gets(str);
 }
-
-
