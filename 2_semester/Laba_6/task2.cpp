8210348688cbdaaa4dd53b3722e9706d9aa6530c
Larri <larand5685@gmail.com>
2021-04-28 16:16:09 +0700

    L6: debug task 2

--- 2_semester/Laba_6/task2.cpp
+++ 2_semester/Laba_6/task2.cpp
@@ -2,32 +2,34 @@
 #include <vector>
 using namespace std;
 
-string decrypt(string messege, string key){
+string decrypt(string messege, string key) {
 	string encrypt_messege;
-	int k = 0;
-	string slovar = "abcdefghijklmnopqrstuvwxyz ";
-	for (char item : messege){
-		encrypt_messege += slovar[(slovar.find(item) - slovar.find(key[k++]))%slovar.size()];
-		if (k == key.size()-1)
+	string slovar = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ/.,|+=-_()*&^%$#@!?1234567890";
+	int k = 0, index, n = slovar.size();
+	for (char item : messege) {
+		if (k == key.size())
 			k = 0;
+		index = (slovar.find(item) - slovar.find(key[k++])) % n;
+		encrypt_messege += slovar[index < 0 ? index + n : index];
 	}
-	return encrypt_messege;   
+	return encrypt_messege;
 }
 
-string encrypt(string messege, string key){
+string encrypt(string messege, string key) {
 	string crypt_messege;
-	int k = 0;
-	string slovar = "abcdefghijklmnopqrstuvwxyz ";
-	for (char item : messege){
-		crypt_messege += slovar[(slovar.find(item) + slovar.find(key[k++]))%slovar.size()];
-		if (k == key.size()-1)
+	int k = 0, index;
+	string slovar = "abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ/.,|+=-_()*&^%$#@!?1234567890";
+	for (char item : messege) {
+		if (k == key.size())
 			k = 0;
+		index = (slovar.find(item) + slovar.find(key[k++])) % slovar.size();
+		crypt_messege += slovar[index];
 	}
-	return crypt_messege;   
+	return crypt_messege;
 }
 
-int main(){
-	string messege = "hello world", key = "a";
+int main() {
+	string messege = "Hello world", key = "Key";
 	cout << messege << endl;
 	cout << encrypt(messege, key) << endl;
 	cout << decrypt(encrypt(messege, key), key) << endl;
