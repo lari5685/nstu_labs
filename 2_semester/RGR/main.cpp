50a25c52054ea7a9fd6558d12bb1b1dfb3b31057
Larri <larand5685@gmail.com>
2021-05-22 23:39:57 +0700

    RGR: added working with files

--- 2_semester/RGR/main.cpp
+++ 2_semester/RGR/main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <cmath>
+#include <vector>
 using namespace std;
 
-enum cryptDo{
+enum cryptDo {
 	encrypt = 1,
 	decrypt = 2
 };
-enum Ciphers{
+
+enum Ciphers {
 	Caesar = 1,
 	Viginer = 2,
 	XOR = 3
 };
 
-int WillDo(string ans){
+int WillDo(string ans) {
 	if ((ans[0] == 'e') || (ans[0] == '1'))
 		return 1;
 	if ((ans[0] == 'd') || (ans[0] == '2'))
@@ -21,22 +24,53 @@ int WillDo(string ans){
 	return 0;
 }
 
-void fileInput(string FileName, string &messege){
+void fileInput(string FileName, string& messege) {
 	string text;
 	ifstream fin(FileName);
-	while(!fin.eof()) {
+	while (!fin.eof()) {
 		fin >> text;
 		messege += text + " ";
 	}
+	fin.close();
 	if (messege.size() > 0)
-		messege.erase(messege.end()-1);  
+		messege.erase(messege.end() - 1);
 }
 
-int mod(int a, int b){
-	return (a%b >= 0) ? a%b : a%b + b;
+void StringToInt(string text, string& ASCIItext) {
+	ASCIItext = "";
+	for (char item : text)
+		ASCIItext += ' ' + to_string(int(item));
+}
+void IntToString(string ASCIItext, string& text) {
+	vector<char> v;
+	text = "";
+	char temp = 0;
+	int k = 0;
+	for (char item : ASCIItext)
+		if (item == ' ') {
+			v.push_back(temp);
+			k = 0; temp = 0;
+		}
+		else
+			temp = (int(item)-'0') + temp * pow(10, k++);
+	if (temp > 0)
+		v.push_back(temp);
+
+	for (char item : v)
+		text += item;
 }
 
-string CaesarCipher(string messege, int key, int WhatDo){// ASCII: 32 - 126
+void fileOutput(string FileName, string& messege) {
+	ofstream fout(FileName);
+	fout << messege;
+	fout.close();
+}
+
+int mod(int a, int b) {
+	return (a % b >= 0) ? a % b : a % b + b;
+}
+
+string CaesarCipher(string messege, int key, int WhatDo) {// ASCII: 32 - 126
 	key *= (WhatDo == decrypt) ? 1 : -1;
 	string resMessege = "";
 	for (char item : messege)
@@ -44,73 +78,88 @@ string CaesarCipher(string messege, int key, int WhatDo){// ASCII: 32 - 126
 	return resMessege;
 }
 
-string ViginerCipher(string messege, string key, int WhatDo){// ASCII: 32 - 126
+string ViginerCipher(string messege, string key, int WhatDo) {// ASCII: 32 - 126
 	int p = (WhatDo == decrypt) ? 1 : -1;
 	string resMessege = "";
 	int k = -1;
 	for (char item : messege)
-		resMessege += char(mod((int)item + p*(int)(char)key[(++k)%key.size()] - 32, 95) + 32);
+		resMessege += char(mod((int)item + p * (int)(char)key[(++k) % key.size()] - 32, 95) + 32);
 	return resMessege;
 }
 
-string XORCipher(string messege, string key){
+string XORCipher(string messege, string key) {
 	int k = -1;
 	string resMessege = "";
-	for (char item : messege){
-		resMessege += item ^ key[(++k)%key.size()];
+	for (char item : messege) {
+		resMessege += item ^ key[(++k) % key.size()];
 	}
 	return resMessege;
 }
 
 
-int main(){
-	cout << " Enter your login password\n";
+int main() {
+	string crypt_message;
+	cout << " Enter your login password:\n ";
 	string ans; cin >> ans;
 	if (ans != "1")
 		return -1;
 
 	cout << " Hello, this program like to decrypt and encrypt your messege.\n";
-	cout << " Enter number cipher method: \n\t1. Caesar.\n\t2. Viginer.\n\t3. XOR.\n";
-	
-	int method = Viginer; cin >> method; 
-	cout << " Good choice!\n What will we do? \n\t1. encrypt\n\t2. decrypt\n";
+	cout << " Enter number cipher method: \n\t1. Caesar.\n\t2. Viginer.\n\t3. XOR.\n\t ... in development\n";
+
+	int method = Viginer; cin >> method;
+	cout << " Good choice!\n What will we do? \n\t1. encrypt\n\t2. decrypt\n ";
 	cin >> ans;
-	while(!WillDo(ans)){
-		cout << "sorry, i didn't understand you, pls enter again\n"; cin >> ans;
+	while (!WillDo(ans)) {
+		cout << "sorry, i didn't understand you, pls enter again\n "; cin >> ans;
 	}
 	int WhatDo = WillDo(ans);
 
 	cout << " Good choice!\n Enter messege from ";
-	(WhatDo == encrypt) ? cout << "encrypt:\n" : cout << "decrypt:\n";
+	(WhatDo == encrypt) ? cout << "encrypt:\n " : cout << "decrypt:\n ";
 	string messege;
-	char FileName[50] = "INPUT.txt";
-	char command[50] = "notepad.exe INPUT.txt";
+	string InputFileName = "INPUT.txt";
+	char command[50] = "notepad.exe ";
+	int k = 12;
+	for (char item : InputFileName)
+		command[k++] = item;
 	system(command);
-	fileInput(FileName, messege);
-	cout << " Good choise!\n";
+	fileInput(InputFileName.c_str(), messege);
+	cout << " Good choise!\n ";
 
-	if (method == Caesar){
-		cout << " Enter key from cipher(number):\n";
+	if (method == Caesar) {
+		cout << " Enter key from cipher(number):\n ";
 		int key_num = 1; cin >> key_num;
-		string crypt_message = CaesarCipher(messege, key_num, WhatDo);
+		crypt_message = CaesarCipher(messege, key_num, WhatDo);
 		cout << " Good job! Your messeg: ";
 		cout << crypt_message << endl;
 	}
-	if (method == Viginer){
-		cout << " Enter key from cipher(string):\n";
+	if (method == Viginer) {
+		cout << " Enter key from cipher(string):\n ";
 		string key_str = "key"; cin >> key_str;
-		string crypt_message = ViginerCipher(messege, key_str, WhatDo);
+		crypt_message = ViginerCipher(messege, key_str, WhatDo);
 		cout << " Good job! Your messeg: ";
 		cout << crypt_message << endl;
 	}
-	if (method == XOR){
-		cout << " Enter key from cipher(string):\n";
+	if (method == XOR) {
+		if (WhatDo == decrypt)
+			IntToString(messege, messege);
+		cout << " Enter key from cipher(string):\n ";
 		string key_str = "key"; cin >> key_str;
-		string crypt_message = XORCipher(messege, key_str);
+		crypt_message = XORCipher(messege, key_str);
+		if (WhatDo == encrypt)
+			StringToInt(crypt_message, crypt_message);
 		cout << " Good job! Your messeg: ";
 		cout << crypt_message << endl;
-		cout << XORCipher(crypt_message, key_str) << endl;
 	}
+	cout << " I write your messege in file: OUTPUT.txt\n";
+
+	k = 12;
+	string OutputFileName = "OUTPUT.txt";
+	fileOutput(OutputFileName.c_str(), crypt_message);
+	for (char item : OutputFileName)
+		command[k++] = item;
+	system(command);
 
 
 }
